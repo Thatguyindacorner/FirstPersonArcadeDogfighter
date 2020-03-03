0f62fe3f0c64cdde8f8412223908e48635bb8525
Michael Miller <miller39@sheridancollege.ca>
2020-03-03 10:47:08 -0500

    Add different potential weapon types
    Add an enumerator that keeps track of the weapon type.
    
    Add a button input to switch between weapon types.

--- DesignWeekWinter2020/Assets/Scripts/Weapon.cs
+++ DesignWeekWinter2020/Assets/Scripts/Weapon.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public GameObject bullet;
+    public GameObject missile;
+
+    enum weaponType {
+        SEMI = 0,
+        AUTO,
+        MISSILE
+    };
+
+    private weaponType gun = weaponType.SEMI;
 
     // Start is called before the first frame update
     void Start()
@@ -15,13 +24,36 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //check to see if we should fire the weapon
+        fireGun();
+
+        if (Input.GetButtonDown("Weapon Switch"))
+            switchGun();
+    }
+
+    void switchGun()
+    {
+        if (gun == weaponType.MISSILE)
+            gun = weaponType.SEMI;
+
+        else
+            gun += 1;
+
+        Debug.Log(gun);
+    }
+
+    void fireGun()
+    {
+        //TODO: split SEMI and AUTO if we actually implement auto...
         if (Input.GetButtonDown("Fire1"))
         {
             Quaternion rotation = transform.rotation;
 
-            Instantiate(bullet, transform.position, rotation);
+            if (gun == weaponType.SEMI || gun == weaponType.AUTO)
+                Instantiate(bullet, transform.position, rotation);
+            else
+                Debug.Log("Insert missile here");
         }
 
-        //TODO: Make it so that whenever the weapon switch button is pressed, the bullet variable is set to the next type of bullet prefab
     }
 }
