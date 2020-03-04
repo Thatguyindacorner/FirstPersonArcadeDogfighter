74f0b89011624f802424725c8ce20574df8ce6ef
Michael Miller <miller39@sheridancollege.ca>
2020-03-04 00:46:34 -0500

    Add basic AI spawning and destructable astroids
    Give obstacles "health" so they can be destroyed.
    
    Make AI spawn like asteroids do.

--- DesignWeekWinter2020/Assets/Scripts/AISpawner.cs
+++ DesignWeekWinter2020/Assets/Scripts/AISpawner.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class AISpawner : MonoBehaviour
 {
+    public GameObject enemy;
+    public GameObject player;
+    Vector3 startPos;
+
+    private int wave;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        wave = 1;
+        startPos = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LevelTracker();
+    }
+
+    void LevelTracker()
+    {
+        if (player.GetComponent<Transform>().position.z - startPos.z > 7500)
+        {
+            print(player.GetComponent<Transform>().position.z - startPos.z);
+            wave += 1;
+            startPos = player.transform.position;
+            SpawnThings();
+        }
+    }
+
+    void SpawnThings()
+    {
+        int n = wave;
+        while (n > 0)
+        {
+            Instantiate(enemy, new Vector3(Random.Range(player.GetComponent<Transform>().position.x, 2000 + player.GetComponent<Transform>().position.x), Random.Range(player.GetComponent<Transform>().position.y - 1000, 1000 + player.GetComponent<Transform>().position.y), Random.Range(player.GetComponent<Transform>().position.z + 2500, player.GetComponent<Transform>().position.z + 10000)), Quaternion.Euler(0, 0, 0));
+            n--;
+            print(Random.Range(player.GetComponent<Transform>().position.x - 1000, 1000 + player.GetComponent<Transform>().position.x));
+        }
     }
 }
---
+++ DesignWeekWinter2020/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int health = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int amt)
+    {
+        Debug.Log("Damaging");
+        health -= amt;
+
+        if (health <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    //colliding with bullets
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Bullet>() != null)
+        {
+            Damage(other.GetComponent<Bullet>().damage);
+            Destroy(other.gameObject);
+        }
+
+    }
+}
