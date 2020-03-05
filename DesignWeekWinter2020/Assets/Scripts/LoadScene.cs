abb767edce39a42fbdffcd0a2123090c75db0a8c
Darryl Feniquito <feniquid@shernet.sheridancollege.ca>
2020-03-05 08:53:18 -0500

    start scene menu

---
+++ DesignWeekWinter2020/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+ 
+   // public AudioSource BellRing;
+
+    // Update is called once per frame
+    public void PlayGame()
+    {
+        print("ClickMe");
+        SceneManager.LoadScene("UI Scene");
+       
+    }
+}
